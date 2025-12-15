458baf39585b01a20b9de347980b5bbb64381ea2
Nargish Gul <nargishgul15@gmail.com>
2025-12-15 20:05:24 +0530

    Update settings and gitignore configuration

--- ems_project/settings.py
+++ ems_project/settings.py
@@ -9,21 +9,23 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+from pathlib import Path
 
 from dotenv import load_dotenv
+
 import os
-from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y7e(mrpomg$@))=+kz=i4(9rdjglnik6*xphxcnob%e)2bo275'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
